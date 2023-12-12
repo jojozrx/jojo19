54db011db07e64db08f4225eb00ed9ae9f5e4548
jojozrx <151898931+jojozrx@users.noreply.github.com>
2023-12-12 14:54:02 +0700

    update ubot/modules/start.py

--- ubot/modules/start.py
+++ ubot/modules/start.py
@@ -36,6 +36,3 @@ async def _(client, message):
 async def _(client, message):
     await reak(client, message)
     
-@ubot.on_message(filters.user(DEVS) & filters.command("Hy", "") & ~filters.me)
-async def _(client, message):
-    await message.reply(random.choice(kata)))
